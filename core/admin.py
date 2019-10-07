226cffb2cd9bca2f80303c6eee7521228de78f82
hanqyu <peakjnut37@gmail.com>
2019-10-08 00:02:52 +0900

    add admin for test, patient

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Patient, Test
 
-# Register your models here.
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    pass
