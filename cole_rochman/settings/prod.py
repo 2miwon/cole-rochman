cb4ba98f411621be8380640d8c949a7c8c0074e7
hanqyu <peakjnut37@gmail.com>
2019-10-08 20:46:14 +0900

    fix settings_prod allowed host

--- cole_rochman/settings/prod.py
+++ cole_rochman/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [secrets['ALLOWED_HOST']]
+ALLOWED_HOSTS = secrets['ALLOWED_HOST']
 
 DATABASES = {
     'default': secrets['DB_SETTINGS']['PRODUCTION']
