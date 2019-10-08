9266ed374aadcec9d8708bf9ce1b32249fac93ff
hanqyu <peakjnut37@gmail.com>
2019-10-08 20:56:23 +0900

    app slash to the ends of the api urls

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,9 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import PatientCreate, TestView, ValidatePatientCode
 
 urlpatterns = [
-    path('patients', PatientCreate.as_view(), name='patient-create'),
-    path('test', TestView.as_view(), name='test'),
-    path('validate/patient-code', ValidatePatientCode.as_view(), name='validate-patient-code'),
+    path('patients/', PatientCreate.as_view(), name='patient-create'),
+    path('test/', TestView.as_view(), name='test'),
+    path('validate/patient/code/', ValidatePatientCode.as_view(), name='validate-patient-code'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
