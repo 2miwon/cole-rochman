9043ed32cf5a805079b6a6f92f7ce7d7df605359
hanqyu <peakjnut37@gmail.com>
2019-10-08 19:39:35 +0900

    add sentry

--- cole_rochman/settings/base.py
+++ cole_rochman/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import json
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -77,6 +79,12 @@ TEMPLATES = [
         },
     },
 ]
+
+sentry_sdk.init(
+    dsn=secrets['SENTRY_ADDRESS'],
+    integrations=[DjangoIntegration()]
+)
+
 
 WSGI_APPLICATION = 'cole_rochman.wsgi.application'
 
