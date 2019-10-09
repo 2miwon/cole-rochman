ab7aceb0d0446a8251a89e7515078829da1f368e
hanqyu <peakjnut37@gmail.com>
2019-10-09 22:59:22 +0900

    fix patient's kakao_user_id field type

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Patient(models.Model):
     code = models.CharField(max_length=20)
-    kakao_user_id = models.IntegerField()
+    kakao_user_id = models.CharField(max_length=150)
     nickname = models.CharField(max_length=20, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
