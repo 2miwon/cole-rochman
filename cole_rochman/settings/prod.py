eea1be3d9eed78f4255f521f485e039786f7adb6
hanqyu <peakjnut37@gmail.com>
2019-10-16 02:27:11 +0900

    (수정) production logging 설정

--- cole_rochman/settings/prod.py
+++ cole_rochman/settings/prod.py
@@ -17,3 +17,25 @@ sentry_sdk.init(
     integrations=[DjangoIntegration()],
     environment='production'
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(PROJECT_DIR, 'settings/debug.log'),
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
