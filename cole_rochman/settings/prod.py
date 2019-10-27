133fc4c653b677c3e36f463cc37e3d763b57d080
hanqyu <peakjnut37@gmail.com>
2019-10-28 00:59:24 +0900

    (수정) 로깅

--- cole_rochman/settings/prod.py
+++ cole_rochman/settings/prod.py
@@ -44,11 +44,6 @@ LOGGING = {
         },
     },
     'loggers': {
-        'django': {
-            'handlers': ['file', 'console', 'SysLog'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
         'django.request': {
             'handlers': ['file', 'console', 'SysLog'],
             'level': 'DEBUG',
