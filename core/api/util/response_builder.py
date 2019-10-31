991ce332076eca0b331f7d7334187fd38f7812aa
hanqyu <peakjnut37@gmail.com>
2019-10-31 21:37:59 +0900

    (추가) response_builder.ResponseBuilder

---
+++ core/api/util/response_builder.py
@@ -0,0 +1,149 @@
+class ResponseBuilder:
+    VALIDATION = 'validation'
+    SKILL = 'skill'
+    version = '2.0'
+
+    def __init__(self, response_type, status=None):
+        """
+        :param response_type: use ResponseBuilder.VALIDATION or ResponseBuilder.SKILL
+        :param args:
+        """
+        if response_type not in [self.VALIDATION, self.SKILL]:
+            raise ValueError('Response_type must be ResponseBuilder.VALIDATION or ResponseBuilder.SKILL')
+        if response_type == self.VALIDATION and status not in ['FAIL', 'SUCCESS']:
+            raise ValueError('Status must be "FAIL" or "SUCCESS" when response_type is VALIDATION')
+
+        self.response_type = response_type
+
+        if response_type == self.VALIDATION:
+            self.status = status
+            self.response = {
+                'status': status
+            }
+        elif response_type == self.SKILL:
+            self.response = {
+                'version': self.version,
+                'template': {
+                    'outputs': []
+                }
+            }
+
+    def __add_outputs(self, data: dict):
+        if self.response_type != self.SKILL:
+            raise ValueError('You cannot use this when response_type is ResponseBuilder.VALIDATION')
+
+        self.response.get('template').get('outputs').append(data)
+
+    def __add_quick_reply(self, data: dict):
+        if self.response_type != self.SKILL:
+            raise ValueError('You cannot use this when response_type is ResponseBuilder.VALIDATION')
+
+        quick_replies = self.response.get('template').get('quickReplies')
+        if quick_replies is None:
+            self.response.get('template')['quickReplies'] = []
+
+        self.response.get('template').get('quickReplies').append(data)
+
+    def add_value(self, value):
+        """
+        :type value: string or int
+        """
+        if self.response_type != self.VALIDATION: raise ValueError(
+            'You cannot use add_value() when response_type is ResponseBuilder.VALIDATION')
+        if not (type(value) == int or type(value) == str): raise ValueError('value should be int or string.')
+
+        self.response['value'] = value
+
+    def add_simple_text(self, message: str):
+        """
+        :param message: It can be up to 1,000 letters.
+        :return: None
+
+        (완성 예제)
+        {
+            "simpleText": {
+                "text": "간단한 텍스트 요소입니다."
+            }
+        }
+        """
+
+        if not (type(message) == int or type(message) == str):
+            raise ValueError('message should be int or string.')
+
+        data = {
+            'simpleText': {
+                'text': message
+            }
+        }
+
+        self.__add_outputs(data=data)
+
+    def add_image(self, image_url: str, alt_text: str):
+        """
+        :param image_url: URL
+        :param alt_text: It can be up to 1,000 letters.
+        :return: None
+
+        (완성 예제)
+        {
+            "simpleImage": {
+                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
+                "altText": "보물상자입니다"
+            }
+        }
+        """
+        if type(image_url) != str and 'http' not in image_url: raise ValueError('image_url should be string of URL.')
+        if type(alt_text) != str: raise ValueError('alt_text should be string')
+
+        data = {
+            'simpleImage': {
+                'imageUrl': image_url,
+                'altText': alt_text
+            }
+        }
+        self.__add_outputs(data=data)
+
+    def add_quick_reply(self, action: str, label: str, block_id: str = None, message_text: str = None):
+        """
+        :type action: string. "block" or "message"
+        :param label: string.
+        :param block_id: string. It is necessary when action is "block"
+        :param message_text: string.
+        :return: None
+
+        (완성 예제)
+        {
+            "action": "block",
+            "label": "예",
+            # "messageText": "예",
+            "blockId": "5da5eac292690d0001a4"
+        }
+        or
+        {
+            "action": "message",
+            "label": "아니요",
+            "messageText": "아니요"
+        }
+
+        """
+        if action is None or label is None: raise ValueError('action or label is None.')
+        if action not in ['block', 'message']: raise ValueError('action should be block or message.')
+        if action == 'block' and block_id is None: raise ValueError('block_id is necessary when action is "block".')
+        if action == 'message' and message_text is None:
+            raise ValueError('message_text is recommended when action is "message".')
+
+        data = {
+            'action': action,
+            'label': label
+        }
+
+        if block_id:
+            data['blockId'] = block_id
+
+        if message_text:
+            data['messageText'] = message_text
+
+        self.__add_quick_reply(data=data)
+
+    def get_response(self):
+        return self.response
