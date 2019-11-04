201145c03793d8104d604f78175003b68e4631b5
hanqyu <peakjnut37@gmail.com>
2019-11-04 17:30:39 +0900

    (추가) Hospital.code

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Patient
+from .models import Patient, Hospital
 
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
     pass
