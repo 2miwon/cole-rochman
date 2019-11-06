b068d49f072e6d969c8c676d0e8b7f61f850f1df
hanqyu <peakjnut37@gmail.com>
2019-11-07 01:32:36 +0900

    (추가) tests.helper.get_context()

--- core/tests/helper/helper.py
+++ core/tests/helper/helper.py
@@ -1,2 +1,10 @@
 def get_first_simple_text(response):
+
     return response.data['template']['outputs'][0]['simpleText']['text']
+
+
+def get_context(response):
+    try:
+        return response.data['context']['values']
+    except KeyError:
+        return None
