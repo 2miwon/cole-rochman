fe93ff4b66131a502d7100f22891071db66535ca
hanqyu <peakjnut37@gmail.com>
2019-11-06 17:06:31 +0900

    (수정) rename validation_hospitals.py from validate_hospitals.py

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from core.api.views.medications import PatientMedicationNotiTimeStart, PatientMedicationNotiSetTime, \
     PatientMedicationNotiReset
 from core.api.views.temp import TempPatientDestroy
-from core.api.views.validate_hospitals import ValidateHospitalCode
+from core.api.views.validation_hospitals import ValidateHospitalCode
 from core.api.views.validation_patients import ValidatePatientNickname, ValidatePatientCode
 from core.api.views.patients import PatientCreate, PatientUpdate
 from core.api.views.validation_visits import ValidateTimeBefore
