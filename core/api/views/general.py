0d9c8110deb3a8377991c41bca89f8c6c6f146a8
hanqyu <peakjnut37@gmail.com>
2019-11-13 14:51:21 +0900

    resolve conflict

--- core/api/views/general.py
+++ core/api/views/general.py
@@ -1,6 +1,6 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from core.api.util.response_builder import ResponseBuilder
 
 
 class ResponseAlwaysOK(APIView):
@@ -8,4 +8,5 @@ class ResponseAlwaysOK(APIView):
     되묻기 질문에서 응답을 받긴 해야 하지만, 응답을 검증하거나 저장할 필요가 없을때 사용합니다.
     """
     def post(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_200_OK)
+        response = ResponseBuilder(response_type=ResponseBuilder.SKILL)
+        return response.get_response_200()
