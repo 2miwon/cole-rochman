a3cd9115f9ad4d2d7170d999e6904fc854c8ee9c
moggaa <55863217+moggaa@users.noreply.github.com>
2019-11-18 02:29:17 +0900

    (수정)admin 창에서 복약,측정 결과 관리하도록 설정 profile 모델 admin에 등록

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Patient, Hospital,MeasurementResult,MedicationResult
-from django.conf.urls import url
-from django.urls import path
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Profile
