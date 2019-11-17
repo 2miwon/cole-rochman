60254dc2dcc7bd9916f07f3293f998cc00fc6dfd
hanqyu <peakjnut37@gmail.com>
2019-11-18 07:45:14 +0900

    (수정) hotfix

--- core/api/views/visits.py
+++ core/api/views/visits.py
@@ -26,7 +26,7 @@ class PatientVisitStart(KakaoResponseAPI):
                 '이미 내원일을 설정하신 적이 있어요.\n현재 설정된 내원 예정일: %s\n\n내원 일정을 수정하시겠습니까?' % patient.next_visiting_date_time_str())
             response.set_quick_replies_yes_or_no(
                 block_id_for_yes='5db314e38192ac000115f9af',  # (블록) 02 치료 관리 재설정_내원 예정일 설정
-                block_id_for_no='5da549bcffa7480001daf821'  # (블록) 치료 관리 설정_시작하기 처음으로
+                block_id_for_no='5d9df7dfb617ea00012b17f3'  # (블록) 치료 관리 설정_내원 관리 종료
             )
             return response.get_response_200()
 
@@ -84,6 +84,7 @@ class PatientVisitDateSet(KakaoResponseAPI):
         response.add_simple_text(text="%s이 내원일이군요." % patient.next_visiting_date_time_str())
         response.add_simple_text(text="내원 알림을 설정할까요?")
         response.set_quick_replies_yes_or_no(block_id_for_yes="5d9df34e92690d0001a458ed",
+                                             # block_id_for_no="",
                                              message_text_for_no="아니요")  # (블록) 03 치료 관리 설정_내원 알림 설정
 
         return response.get_response_200()
