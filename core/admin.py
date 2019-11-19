26e5eaeb1c7e085d621883fdd3b1ba9a42522e28
hanqyu <peakjnut37@gmail.com>
2019-11-19 23:02:05 +0900

    (추가) PatientAdmin filtering

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Patient, Hospital,MeasurementResult,MedicationResult
+from .models import Patient, Hospital, MeasurementResult, MedicationResult
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Profile
@@ -8,7 +8,17 @@ from .models import Profile
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('code', 'hospital', 'kakao_user_id', 'nickname', 'daily_medication_count', 'medication_noti_time_1',
+                    'medication_noti_time_2', 'medication_noti_time_3', 'medication_noti_time_4',
+                    'medication_noti_time_5', 'next_visiting_date_time')
+    search_fields = (
+        'patient__code',
+        'patient__kakao_user_id',
+    )
+    list_filter = [
+        'hospital', 'medication_manage_flag', 'measurement_manage_flag', 'visit_manage_flag',
+        'medication_noti_flag', 'visit_notification_flag', 'measurement_noti_flag'
+    ]
 
 
 @admin.register(Hospital)
@@ -23,15 +33,17 @@ class ProfileInline(admin.StackedInline):
 
 
 class UserAdmin(BaseUserAdmin):
-    inlines = (ProfileInline, )
+    inlines = (ProfileInline,)
 
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
+
 @admin.register(MeasurementResult)
 class MeasurementResult(admin.ModelAdmin):
     pass
+
 
 @admin.register(MedicationResult)
 class MeadicationResult(admin.ModelAdmin):
