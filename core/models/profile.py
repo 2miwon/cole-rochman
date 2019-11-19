5ac6111a268f7b4754f1a13b4b5e8eae7ce29bfa
hanqyu <peakjnut37@gmail.com>
2019-11-19 15:23:52 +0900

    (chore) line alignments

--- core/models/profile.py
+++ core/models/profile.py
@@ -1,14 +1,5 @@
-import datetime
-from enum import Enum
-
-from django.contrib.postgres.fields import JSONField
-from django.db import models
-from datetime import timedelta
 from django.contrib.auth.models import User
-
-from core.models.helper.helper import EnumField
-from core.serializers import NotificationRecordSerializer
-
+from django.db import models
 
 
 class Profile(models.Model):
--- core/web/urls.py
+++ core/web/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from core.web import views
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('menu',views.web_menu, name='web_menu'),
+    path('menu', views.web_menu, name='web_menu'),
     path('dashboard', views.user_dashboard, name='user_dashboard'),
     path('dashboard/<int:pid>', views.patient_status, name='patient_status'),
-
     path('login', views.sign_in, name='login_user'),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout",),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout", ),
 
 ]
