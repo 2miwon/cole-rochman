fe7b04f80379104e04402d9ddf535de409316c42
moggaa <55863217+moggaa@users.noreply.github.com>
2019-11-28 11:16:20 +0900

    Dashboardbranch3 (#12)
    * (수정) 로그인창 배경 사진 보이게
    
    * (수정) 예외 처리 디버깅
    (추가) 복약 결과 창 (표 형식)
    
    * (추가) 인덱스 창 (footer: 사업자등록번호)
    (추가) 산소포화도 결과 (스타일 신경 못씀)
    
    * (수정) 예외처리 추가 (대쉬보드 디버깅)
    
    * (수정) 예외처리 추가
    (추가) 치료 예정 종료일 자동 설정\
    (추가) 산소포화도 결과창 추가
    
    * (수정) 환자 치료 과정 퍼센트 오류 디버깅
    
    * (수정) 환자 치료 과정 퍼센트 오류 디버깅
    (추가) 산소포화도 색깔(원)
    
    * (추가) 산소포화도 결과
    
    * (추가) 산소포화도 결과
    
    * (수정) 산소포화도 결과
    
    * (수정) zerofill
    
    * (추가) 엑셀파일 환자 데이터 import export 기구현(admin 페이지에서 가능함)
    
    import할 엑셀 파일 형식을 잡았음
    
    requirement에 import export 라이브러리 추가
    
    * (수정) 메뉴 변경

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
-
+from import_export.admin import ImportExportModelAdmin
 from .models import Patient, Hospital, MeasurementResult, MedicationResult
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Profile
+from import_export.widgets import ForeignKeyWidget
+from import_export import resources,fields
+
+
+class PatientResource(resources.ModelResource):
+    hospital = fields.Field(
+        column_name='hospital',
+        attribute='hospital',
+        widget=ForeignKeyWidget(Hospital, 'code'))
+    user = fields.Field(
+        column_name='user',
+        attribute='user',
+        widget=ForeignKeyWidget(User, 'username'))
+
+
+    class Meta:
+        model = Patient
+
+        fields = ('id','code','hospital','phone_number','name','user')
 
 
 @admin.register(Patient)
-class PatientAdmin(GuardedModelAdmin):
+class PatientAdmin(GuardedModelAdmin,ImportExportModelAdmin):
+    resource_class =PatientResource
+
     user_can_access_owned_objects_only = True
 
     list_display = (
