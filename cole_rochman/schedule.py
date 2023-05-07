afbbe6e1835dbf10af988c3de634004b79c9d253
0921sean <0921sean@example.com>
2023-05-07 13:35:54 +0000

    [05.07]

--- cole_rochman/schedule.py
+++ cole_rochman/schedule.py
@@ -22,17 +22,17 @@ QUEUE_NOTIFICATION = {
 SCHEDULE = {
     'create-morning-notification-every-12-30-am': {
         'task': 'core.tasks.notification.create_morning_notification',
-        'schedule': crontab(minute=40, hour=7),
+        'schedule': crontab(minute=30, hour=0),
         'options': {**RETRY_OPTIONS, **QUEUE_NOTIFICATION}
     },
     'create-medication-notification-every-12-40-am': {
         'task': 'core.tasks.notification.create_medication_notification',
-        'schedule': crontab(minute=50, hour=7),
+        'schedule': crontab(minute=40, hour=0),
         'options': {**RETRY_OPTIONS, **QUEUE_NOTIFICATION}
     },
     'create-visit-notification-every-12-40-am': {
         'task': 'core.tasks.notification.create_visit_notification',
-        'schedule': crontab(minute=50, hour=7),
+        'schedule': crontab(minute=40, hour=0),
         'options': {**RETRY_OPTIONS, **QUEUE_NOTIFICATION}
     }
 }
@@ -41,14 +41,14 @@ if settings.AUTO_SEND_NOTIFICAITON:
     SCHEDULE.update({
         'send-notification-every-1-minutes': {
             'task': 'core.tasks.notification.send_notifications',
-            'schedule': crontab(minute='*/1', hour='7-19'),
+            'schedule': crontab(),
             'options': {**RETRY_OPTIONS, **QUEUE_NOTIFICATION}
         }
     })
-    SCHEDULE.update({
-        'send-notification-at=8-pm': {
-            'task': 'core.tasks.notification.send_notifications',
-            'schedule': crontab(minute=0, hour=20),
-            'options': {**RETRY_OPTIONS, **QUEUE_NOTIFICATION}
-        }
-    })
+    # SCHEDULE.update({
+    #     'send-notification-at=8-pm': {
+    #         'task': 'core.tasks.notification.send_notifications',
+    #         'schedule': crontab(minute=0, hour=20),
+    #         'options': {**RETRY_OPTIONS, **QUEUE_NOTIFICATION}
+    #     }
+    # })
