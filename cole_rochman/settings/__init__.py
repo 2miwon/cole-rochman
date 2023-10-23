b04d4fef6442075e02a7c069995baadcc6281ac0
2miwon <zhengsfsf@gmail.com>
2023-10-24 07:02:56 +0900

    세팅 수정

--- cole_rochman/settings/__init__.py
+++ cole_rochman/settings/__init__.py
@@ -2,9 +2,7 @@ import os
 SETTINGS_MODULE = os.environ.get('DJANGO_SETTINGS_MODULE')
 STATIC_ROOT = '/var/www/cole-rochman/static'
 
-if not SETTINGS_MODULE or SETTINGS_MODULE == 'cole_rochman.settings':
+if SETTINGS_MODULE == 'cole_rochman.settings.dev':
     from .dev import *
-# elif SETTINGS_MODULE == 'cole_rochman.settings.prod':
-#     from .prod import *
 else:
     from .prod import *
