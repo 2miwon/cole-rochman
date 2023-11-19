82b2dd55cb5a782a2fcf04125106c0f677be51f1
2miwon <zhengsfsf@gmail.com>
2023-11-19 13:37:00 +0900

    error page 2

--- core/views.py
+++ core/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 
 
 def error400(request, exception):
-    return render(request, "error.html", context={"code": 400}, status=404)
+    context = {"code": 400, "message": "Bad Request"}
+    return render(request, "error.html", context=context, status=404)
 
 
 def error404(request, exception):
-    return render(request, "error.html", context={"code": 404}, status=404)
+    context = {
+        "code": 404,
+        "message": "Page Not Found",
+    }
+    return render(request, "error.html", context=context, status=404)
 
 
 def error500(request):
-    return render(request, "error.html", context={"code": 500}, status=500)
+    context = {
+        "code": 500,
+        "message": "Internal Server Error",
+    }
+    return render(request, "error.html", context=context, status=500)
