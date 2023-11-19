a91ab674d85bc3963b95bcf00ac0e106cdf03470
2miwon <zhengsfsf@gmail.com>
2023-11-19 11:38:48 +0900

    refactor + css error

--- cole_rochman/settings/prod.py
+++ cole_rochman/settings/prod.py
@@ -4,58 +4,56 @@ from .base import *
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = secrets.get('ALLOWED_HOST')
+ALLOWED_HOSTS = secrets.get("ALLOWED_HOST")
 
-DATABASES = {
-    'default': secrets.get('DB_SETTINGS')#.get('TEST')
-}
+DATABASES = {"default": secrets.get("DB_SETTINGS")}  # .get('TEST')
 
 # noinspection PyUnresolvedReferences
 
-STATIC_ROOT = '/var/www/cole-rochman/static'
+STATIC_ROOT = "/var/www/cole-rochman/static"
 
 sentry_sdk.init(
-    dsn=secrets.get('SENTRY_ADDRESS'),
+    dsn=secrets.get("SENTRY_ADDRESS"),
     integrations=[DjangoIntegration()],
-    environment='production'
+    environment="production",
 )
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'simple': {
-            'format': '%(asctime)s SENDER_NAME PROGRAM_NAME: %(message)s',
-            'datefmt': '%Y-%m-%dT%H:%M:%S',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s SENDER_NAME PROGRAM_NAME: %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S",
         },
     },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(PROJECT_DIR, 'settings/debug.log'),
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(PROJECT_DIR, "settings/debug.log"),
         },
-        'console': {
-            'class': 'logging.StreamHandler',
+        "console": {
+            "class": "logging.StreamHandler",
         },
-        'SysLog': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.SysLogHandler',
-            'formatter': 'simple',
-            'address': secrets.get('PAPERTRAIL_ADDRESS')
+        "SysLog": {
+            "level": "DEBUG",
+            "class": "logging.handlers.SysLogHandler",
+            "formatter": "simple",
+            "address": secrets.get("PAPERTRAIL_ADDRESS"),
         },
     },
-    'loggers': {
-        'django.request': {
-            'handlers': ['file', 'SysLog'],
-            'level': 'DEBUG',
-            'propagate': True,
+    "loggers": {
+        "django.request": {
+            "handlers": ["file", "SysLog"],
+            "level": "DEBUG",
+            "propagate": True,
         },
     },
 }
 
-BROKER_URL = secrets.get('BROKER_URL')
+BROKER_URL = secrets.get("BROKER_URL")
 
 AUTO_SEND_NOTIFICAITON = True
