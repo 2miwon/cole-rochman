0aaf93813761604e0d9b9fa0b9507a1105b86087
2miwon <zhengsfsf@gmail.com>
2023-12-01 04:23:21 +0900

    .

--- cole_rochman/schedule.py
+++ cole_rochman/schedule.py
@@ -24,7 +24,7 @@ SCHEDULE = {
     # },
     "create-medication-notification-every-12-40-am": {
         "task": "core.tasks.notification.create_medication_notification",
-        "schedule": crontab(minute=40, hour=0),
+        "schedule": crontab(minute=30, hour=4),
         "options": {**RETRY_OPTIONS, **QUEUE_NOTIFICATION},
     },
     "create-visit-notification-every-12-40-am": {
--- cole_rochman/settings/celery.py
+++ cole_rochman/settings/celery.py
@@ -4,23 +4,21 @@ from cole_rochman.schedule import SCHEDULE
 
 enable_utc = False
 
-timezone = 'Asia/Seoul'
+timezone = "Asia/Seoul"
 
-task_soft_time_limit = 60 * 2
+task_soft_time_limit = 60
 
 # QUEUE SETTINGS
 broker_transport_options = {
-    'priority_steps': list(range(4)),  # 0: Very High / 1: High / 2: Middle / 3: Low
+    "priority_steps": list(range(4)),  # 0: Very High / 1: High / 2: Middle / 3: Low
 }
 
-task_default_queue = 'default'
-task_default_exchange_type = 'direct'
-task_default_routing_key = 'default'
+task_default_queue = "default"
+task_default_exchange_type = "direct"
+task_default_routing_key = "default"
 
-default_exchange = Exchange('default', type='direct')
+default_exchange = Exchange("default", type="direct")
 
-task_queues = (
-    Queue('default'),
-)
+task_queues = (Queue("default"),)
 
 beat_schedule = SCHEDULE
