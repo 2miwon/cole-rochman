2ef345fda997b947b0d60fd550a4de2495483549
2miwon <zhengsfsf@gmail.com>
2023-12-02 02:24:07 +0900

    packaging debug 3

--- core/tasks/__init__.py
+++ core/tasks/__init__.py
@@ -1,2 +1,6 @@
-from .notification import create_medication_notification, create_visit_notification, send_notifications
-from .tasks import create_morning_notification,
+from .notification import (
+    create_medication_notification,
+    create_visit_notification,
+    send_notifications,
+)
+from .tasks import create_morning_notification
